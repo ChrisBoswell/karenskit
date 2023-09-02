038b9301aff4f7492a5e6a701779c978f92b0932
Christian Boswell <christianm.boswell@gmail.com>
2023-09-02 12:26:20 -0500

    new video

--- karenskit/src/views/home.vue
+++ karenskit/src/views/home.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="video-container-mobile lg:video-container xl:video-container -mt-4 h-1/2 overflow-hidden">
         <video class="w-screen -mt-1" autoplay muted loop>
-            <source src="../assets/temp-video.mp4" type="video/mp4">
+            <source src="../assets/karenkit_intro.mp4" type="video/mp4">
+            <!-- <source src="../assets/karenkit_intro.mov.mov" type="video/quicktime"> -->
         </video>
     </div>
     <div class="flex mt-6">
